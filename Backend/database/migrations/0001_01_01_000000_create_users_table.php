5669f75ae34a5048ad27596784d07c07e1b0b29f
sahadat <sahadatappdev@gmail.com>
2026-07-19 11:30:06 +0600

    Feat(Schema) Create role and status column in user table

--- Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,14 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            
+            $table->enum('role', [
+                'admin',
+                'employee'
+            ])->default('employee');
+
+            $table->boolean('is_active')->default(true);
+
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
