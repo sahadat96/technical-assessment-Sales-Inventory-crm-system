caa077ee96dfea65c030c718626a175539687eaf
sahadat <sahadatappdev@gmail.com>
2026-07-19 17:29:15 +0600

    Feat(Customer Relation Management): Create CustomerAssignmemnt, CustomerCommunications Model and migration

---
+++ Backend/database/migrations/2026_07_19_112322_create_customer_assignments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_assignments', function (Blueprint $table) {
+            $$table->id();
+
+            $table->foreignId('customer_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->timestamp('assigned_at');
+
+            $table->enum('status', [
+                'assigned',
+                'completed',
+                'cancelled'
+            ])->default('assigned');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_assignments');
+    }
+};
---
+++ Backend/database/migrations/2026_07_19_112549_create_customer_communications_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customer_communications', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('customer_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
+
+            $table->enum('type', [
+                'email',
+                'sms'
+            ]);
+
+            $table->string('subject')->nullable();
+            $table->text('message');
+            
+            $table->enum('status', [
+                'pending',
+                'sent',
+                'failed'
+            ])->default('pending');
+
+            $table->timestamp('sent_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customer_communications');
+    }
+};
